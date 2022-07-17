1ce71f040300495f018096dccd23b47f84a87529
Joyonto <joyonto,jatri@gmail.com>
2022-07-17 13:34:52 +0600

    starter pack upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        corporate: '#F04935',
+        corporateDark: '#212121',
+        corporateDarkText: '#A6A69E',
+        corporateLight: '#333',
+      },
+    },
+  },
+  plugins: [],
+};
